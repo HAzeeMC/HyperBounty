0d2072166656e37767316312c929229bce074b00
HAzeeMC <nt5974649@gmail.com>
2025-10-14 13:31:37 +0700

    Update GUIBuilder.java

--- src/main/java/com/hazee/hyperbounty/gui/GUIBuilder.java
+++ src/main/java/com/hazee/hyperbounty/gui/GUIBuilder.java
@@ -6,6 +6,7 @@ import com.hazee.hyperbounty.manager.CooldownManager;
 import com.hazee.hyperbounty.manager.KillstreakManager;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
+import org.bukkit.entity.Player; // THÊM IMPORT NÀY
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
