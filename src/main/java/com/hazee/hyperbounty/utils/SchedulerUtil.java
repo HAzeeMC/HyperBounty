23830ab3e9e26b302a0f8405932f118115960c10
HAzeeMC <nt5974649@gmail.com>
2025-10-14 12:00:39 +0700

    Create SchedulerUtil.java

---
+++ src/main/java/com/hazee/hyperbounty/utils/SchedulerUtil.java
@@ -0,0 +1,102 @@
+package com.hazee.hyperbounty.utils;
+
+import com.hazee.hyperbounty.HyperBounty;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+
+public class SchedulerUtil {
+    
+    private static Boolean isFolia = null;
+    
+    public static boolean isFolia() {
+        if (isFolia == null) {
+            try {
+                Class.forName("io.papermc.paper.threadedregions.RegionizedServer");
+                isFolia = true;
+            } catch (ClassNotFoundException e) {
+                isFolia = false;
+            }
+        }
+        return isFolia;
+    }
+    
+    public static void runTask(JavaPlugin plugin, Runnable task) {
+        if (isFolia()) {
+            Bukkit.getGlobalRegionScheduler().run(plugin, t -> task.run());
+        } else {
+            Bukkit.getScheduler().runTask(plugin, task);
+        }
+    }
+    
+    public static void runTask(JavaPlugin plugin, Entity entity, Runnable task) {
+        if (isFolia()) {
+            entity.getScheduler().run(plugin, t -> task.run(), null);
+        } else {
+            Bukkit.getScheduler().runTask(plugin, task);
+        }
+    }
+    
+    public static void runTaskLater(JavaPlugin plugin, Runnable task, long delay) {
+        if (isFolia()) {
+            Bukkit.getGlobalRegionScheduler().runDelayed(plugin, t -> task.run(), delay);
+        } else {
+            Bukkit.getScheduler().runTaskLater(plugin, task, delay);
+        }
+    }
+    
+    public static void runTaskLater(JavaPlugin plugin, Entity entity, Runnable task, long delay) {
+        if (isFolia()) {
+            entity.getScheduler().runDelayed(plugin, t -> task.run(), null, delay);
+        } else {
+            Bukkit.getScheduler().runTaskLater(plugin, task, delay);
+        }
+    }
+    
+    public static void runTaskTimer(JavaPlugin plugin, Runnable task, long delay, long period) {
+        if (isFolia()) {
+            Bukkit.getGlobalRegionScheduler().runAtFixedRate(plugin, t -> task.run(), delay, period);
+        } else {
+            Bukkit.getScheduler().runTaskTimer(plugin, task, delay, period);
+        }
+    }
+    
+    public static BukkitTask runTaskTimer(JavaPlugin plugin, Entity entity, Consumer<BukkitTask> task, long delay, long period) {
+        if (isFolia()) {
+            entity.getScheduler().runAtFixedRate(plugin, t -> task.accept(t), null, delay, period);
+            return null;
+        } else {
+            return Bukkit.getScheduler().runTaskTimer(plugin, task, delay, period);
+        }
+    }
+    
+    public static void runAsync(JavaPlugin plugin, Runnable task) {
+        if (isFolia()) {
+            Bukkit.getAsyncScheduler().runNow(plugin, t -> task.run());
+        } else {
+            Bukkit.getScheduler().runTaskAsynchronously(plugin, task);
+        }
+    }
+    
+    public static <T> CompletableFuture<T> runAsync(JavaPlugin plugin, java.util.function.Supplier<T> supplier) {
+        CompletableFuture<T> future = new CompletableFuture<>();
+        runAsync(plugin, () -> {
+            try {
+                future.complete(supplier.get());
+            } catch (Exception e) {
+                future.completeExceptionally(e);
+            }
+        });
+        return future;
+    }
+    
+    public static void cancelTask(BukkitTask task) {
+        if (task != null && !isFolia()) {
+            task.cancel();
+        }
+    }
+}
